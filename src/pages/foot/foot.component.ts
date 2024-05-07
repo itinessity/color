4e3284abe858f7a44b42eb36a86f92d165ba6ec7
Itinessity <mail@example.org>
2024-05-07 07:19:29 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/itinessity/color/commit/be7d218c66a2a8de5a7c00b97cf6ae76ca21aa46

--- src/pages/foot/foot.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-@Component({
-  selector: 'app-foot',
-  templateUrl: './foot.html',
-  styleUrls: ['./foot.css']
-})
-export class FootComponent {
-}
--- src/pages/menu/menu.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-@Component({
-  selector: 'app-menu',
-  templateUrl: './menu.html',
-  styleUrls: ['./menu.css']
-})
-export class MenuComponent {
-}
