ff64e3ce4131d71fe9afc199d55e00a5ec1e2614
Eugenia_Tamasyan <itinessity@gmail.com>
2024-06-16 12:16:34 -0600

    new version

--- src/pages/movies/MovieComponent.ts
+++ src/pages/movies/MovieComponent.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {FormControl, FormGroupDirective, NgForm, Validators} from "@angular/forms";
-import {MatSnackBar} from "@angular/material/snack-bar";
-import {ErrorStateMatcher} from "@angular/material/core";
 
 @Component({
   selector: 'app-pages-movie',
@@ -10,57 +6,4 @@ import {ErrorStateMatcher} from "@angular/material/core";
   styleUrls: ['./movie.css']
 })
 export class MovieComponent {
-  submitted = false;
-  mailChimpEndpoint = 'https://the-banished.us21.list-manage.com/subscribe/post-json?u=aa13a26086f34e69f3e1a6c85&amp;id=433d5d30ee&amp;f_id=007ef0e6f0';
-
-  constructor(private http: HttpClient, private _snackBar: MatSnackBar) { }
-
-  // reactive form components
-  emailControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
-
-  matcher = new MyErrorStateMatcher();
-  durationInSeconds = 3;
-
-  async submit() {
-    if (this.emailControl.status === 'VALID') {
-      const params = new HttpParams()
-        .set('EMAIL', this.emailControl.value ?? "")
-        .set('b_aa13a26086f34e69f3e1a6c85_9c172380ae', '');
-
-      const mailChimpUrl = this.mailChimpEndpoint + "&"+ params.toString();
-
-      this.http.jsonp<MailChimpResponse>(mailChimpUrl, 'c')
-        .subscribe(response => {
-          if (response.result && response.result !== 'error') {
-            this.submitted = true;
-            this.emailControl.setValue("");
-
-            this._snackBar.open(response.msg, "Close", {
-              duration: this.durationInSeconds * 1000,
-            });
-          }
-          else {
-            this._snackBar.open(response.msg, "Close", {
-              duration: this.durationInSeconds * 1000,
-            });
-          }
-        });
-    }
-  }
-}
-
-interface MailChimpResponse {
-  result: string;
-  msg: string;
-}
-
-
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
 }
